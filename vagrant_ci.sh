c1f1ea495e386bc11e23c46f06b3ab7f76f893c5
Health Lab <healthlab@macmini.mitre.org>
2013-04-25 15:30:51 -0400

    Minor tweaks - remove plist log after emailing it

--- vagrant_ci.sh
+++ vagrant_ci.sh
@@ -10,14 +10,14 @@ VAGRANT_LOG=info vagrant up --provider=vmware_fusion >> /Users/healthlab/ci_vm/c
 cd /Users/healthlab/ci_vm/ci_poll
 if grep failed /Users/healthlab/ci_vm/ci_poll/vagrant_ci.log
 then
-    ruby email.rb failure >> /Users/healthlab/ci_vm/ci_poll/vagrant_ci.log
+    /Users/healthlab/.rvm/rubies/ruby-1.9.3-p392/bin/ruby email.rb failure >> /Users/healthlab/ci_vm/ci_poll/vagrant_ci.log
     echo "Failure email sent" >> /Users/healthlab/ci_vm/ci_poll/vagrant_ci.log
 else
-	ruby email.rb success >> /Users/healthlab/ci_vm/ci_poll/vagrant_ci.log
+	/Users/healthlab/.rvm/rubies/ruby-1.9.3-p392/bin/ruby email.rb success >> /Users/healthlab/ci_vm/ci_poll/vagrant_ci.log
 	echo "Success email sent" >> /Users/healthlab/ci_vm/ci_poll/vagrant_ci.log
 fi
 
-rm /Users/healthlab/ci_vm/ci_poll/vagrant.ci.plist.log
-
 cd /Users/healthlab/ci_vm/test
 vagrant destroy
+
+rm /Users/healthlab/ci_vm/ci_poll/vagrant.ci.plist.log
