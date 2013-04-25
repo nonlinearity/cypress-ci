21ae437495107f95336c987750dec43e67d9a1a7
Health Lab <healthlab@macmini.mitre.org>
2013-04-25 14:25:11 -0400

    Updating logging in vagrant_ci.sh

--- vagrant_ci.sh
+++ vagrant_ci.sh
@@ -10,12 +10,11 @@ VAGRANT_LOG=info vagrant up --provider=vmware_fusion >> /Users/healthlab/ci_vm/c
 cd /Users/healthlab/ci_vm/ci_poll
 if grep failed /Users/healthlab/ci_vm/ci_poll/vagrant_ci.log
 then
-    ruby email.rb failure
+    ruby email.rb failure >> /Users/healthlab/ci_vm/ci_poll/vagrant_ci.log
     echo "Failure email sent" >> /Users/healthlab/ci_vm/ci_poll/vagrant_ci.log
 else
-	ruby email.rb success
+	ruby email.rb success >> /Users/healthlab/ci_vm/ci_poll/vagrant_ci.log
 	echo "Success email sent" >> /Users/healthlab/ci_vm/ci_poll/vagrant_ci.log
-
 fi
 
 rm /Users/healthlab/ci_vm/ci_poll/vagrant.ci.plist.log
